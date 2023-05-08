6184980891f96ccd8e8d15f52723c45f2b2d18c7
JustynaGargula <s221391@student.uek.krakow.pl>
2023-05-08 17:53:28 +0200

    small update

--- my-ecommerce/src/test/java/pl/justyna/productCatalog/ProductCatalogHTTPTest.java
+++ my-ecommerce/src/test/java/pl/justyna/productCatalog/ProductCatalogHTTPTest.java
@@ -3,6 +3,8 @@ package pl.justyna.productCatalog;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -15,20 +17,18 @@ public class ProductCatalogHTTPTest {
     @Autowired
     TestRestTemplate http;
 
-    @Autowired
-    ProductCatalog catalog;
 
     @Test
     void itLoadsIndex(){
-        String url = String.format("https://localhost:%s", port);
+        String url = String.format("https://localhost:%s", this.port);
         ResponseEntity<String> resp = http.getForEntity(url, String.class);
         assert resp.getStatusCode().equals(HttpStatus.OK);                    //test funkcjonalny
     }
 
     @Test
     void itLoadsProducts(){
-        String url = String.format("https://localhost:%s", port);
-        ResponseEntity<String> resp = http.getForEntity(url, String.class);
+        String url = String.format("https://localhost:%s", this.port);
+        ResponseEntity<Product[]> resp = http.getForEntity(url, Product[].class);
         assert resp.getStatusCode().equals(HttpStatus.OK);
     }
 
